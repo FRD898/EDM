fd7e507f9fcb4f1bebbc4caa0deced49fb0bcbbe
Freider Achic <freider8998@gmail.com>
2022-05-17 23:10:07 -0500

    randomForest added

--- app/server/app.py
+++ app/server/app.py
@@ -35,22 +35,71 @@ def convertDataFrame(frontData):
 
 @app.route('/search')
 def searchDocs():
-    svm = pr.Cargar("svm")
+    svm = pr.Cargar("randomForest2")
     query = request.args.get('data')
     data = convertDataFrame(query)
     df = pr.prepareData(data)        
-    atributos = ['38','28','37','26','34','24','29']
+    atributos = ['38','28','37','26','34','24','29','25','19']
+    
+    '''
+    Regresión Lineal
+
+    '38' nota del examen parcial en el curso de Introducción a la Computación
+    '28' presentar todas las tareas antes del parcial
+    '24' Rindió la primera práctica en el curso de Introducción a la Computación
+    '37' promedio de practicas Intro a C.
+    '26' Rindio el examen parcial de Intro a C.
+    '34' nota del examen parcial en el curso de Algebra Lineal
+    '29' promedio de prácticas antes del parcial en el curso de Química 1
+    '25' Rindió la segunda práctica en el curso de Introducción a la Computación
+    '19' En qué sección estuvo matriculado
+
+    PCA
+
+    35: promedio de prácticas antes del parcial en el curso de Cálculo Diferencial	
+    11: Hizo traslado interno o externo
+    25: Rindió la segunda práctica en el curso de Introducción a la Computación
+    8: Nivel de inglés
+    5: Vivía solo o con su familia
+    19: sección
+    17: Tipo de ingreso
+    9: segunda carrera universitaria
+    10: Estudió en algún instituto antes de la universidad
+    36: nota del examen parcial en el curso de Cálculo Diferencial
+    20: Formó parte de algún grupo de deportes
+    23: Porcentaje de faltas
+    29: Promedio de PCs de Quimica antes del parcial
+    6: Lugar de residencia
+
+    38, nota del examen parcial en el curso de Introducción a la Computación
+    28, nota del examen parcial en el curso de Introducción a la Computación
+    37, promedio de prácticas antes del parcial  
+    26, Rindió la segunda práctica en el curso de Introducción a la Computación
+    34, nota del examen parcial en el curso de Algebra Lineal
+    24, Rindió la primera práctica en el curso de Introducción a la Computación
+    29, promedio de prácticas antes del parcial en el curso de Química 1
+
+    Matriz de correlación clasificación
+
+    '''
     X = df[atributos]
     y_pred = svm.predict(X)
     res = []
-    for val in y_pred:
+    real = []
+    for i,val in enumerate(y_pred):
         print(f'val {val}')
         if val==0.0:
             res.append("desaprobará")
         else:
             res.append("aprobará")
+        if df['18'][i]==0.0:
+            real.append("desaprobará")
+        else:
+            real.append("aprobará")
     res = {
-        "prediccion": res
+        "prediccion": res,
+        "real": real
+
     }
     return jsonify(res)
 
